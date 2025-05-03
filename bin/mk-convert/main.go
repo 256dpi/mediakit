b22f7e86d86f73a9125f1bc3d7dbdd2280cb9487
Joël Gähwiler <joel.gaehwiler@gmail.com>
2025-05-03 18:24:20 +0200

    added flags to convert tool

--- bin/mk-convert/main.go
+++ bin/mk-convert/main.go
@@ -6,14 +6,17 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/256dpi/xo"
+	"github.com/kr/pretty"
+
 	"github.com/256dpi/mediakit"
 	"github.com/256dpi/mediakit/ffmpeg"
 	"github.com/256dpi/mediakit/vips"
-	"github.com/256dpi/xo"
-	"github.com/kr/pretty"
 )
 
 var mode = flag.String("mode", "video", "")
+var preset = flag.Int("preset", 0, "")
+var width = flag.Int("width", 300, "")
 
 func main() {
 	// parse flags
@@ -46,23 +49,35 @@ func main() {
 	// convert input
 	switch *mode {
 	case "image":
-		err = mediakit.ConvertImage(nil, input, output, ffmpeg.ImageJPEG, mediakit.MaxWidth(300))
+		if *preset == 0 {
+			*preset = int(vips.JPGWeb)
+		}
+		err = mediakit.ConvertImage(nil, input, output, vips.Preset(*preset), mediakit.MaxWidth(*width))
 	case "audio":
-		err = mediakit.ConvertAudio(nil, input, output, ffmpeg.AudioMP3VBRStandard, 48000, &mediakit.Progress{
+		if *preset == 0 {
+			*preset = int(ffmpeg.AudioMP3VBRStandard)
+		}
+		err = mediakit.ConvertAudio(nil, input, output, ffmpeg.Preset(*preset), 48000, &mediakit.Progress{
 			Rate: time.Second,
 			Func: func(progress float64) {
 				pretty.Println(progress)
 			},
 		})
 	case "video":
-		err = mediakit.ConvertVideo(nil, input, output, ffmpeg.VideoMP4H264AACFast, mediakit.MaxWidth(300), 30, 48000, &mediakit.Progress{
+		if *preset == 0 {
+			*preset = int(ffmpeg.VideoMP4H264AACFast)
+		}
+		err = mediakit.ConvertVideo(nil, input, output, ffmpeg.Preset(*preset), mediakit.MaxWidth(*width), 30, 48000, &mediakit.Progress{
 			Rate: time.Second,
 			Func: func(progress float64) {
 				pretty.Println(progress)
 			},
 		})
 	case "extract":
-		err = mediakit.ExtractImage(nil, input, temporary, output, 0.25, vips.JPGWeb, mediakit.MaxWidth(300))
+		if *preset == 0 {
+			*preset = int(vips.JPGWeb)
+		}
+		err = mediakit.ExtractImage(nil, input, temporary, output, 0.25, vips.Preset(*preset), mediakit.MaxWidth(*width))
 	default:
 		panic("unknown mode: " + *mode)
 	}
