30329039d14c76c839d5527c49c1407f0fa2c6d4
Joël Gähwiler <joel.gaehwiler@gmail.com>
2025-05-11 21:06:51 +0200

    removed long test again

--- chromium/screenshot_test.go
+++ chromium/screenshot_test.go
@@ -45,18 +45,4 @@ func TestScreenshot(t *testing.T) {
 		Min: image.Point{},
 		Max: image.Point{X: 3840, Y: 2160},
 	}, img.Bounds())
-
-	buf, err = Screenshot(ctx, "https://en.wikipedia.org/wiki/Chromium", ScreenshotOptions{
-		Width:  1280,
-		Height: 720,
-		Scale:  1,
-		Full:   true,
-	})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, buf)
-
-	img, err = png.Decode(bytes.NewReader(buf))
-	assert.NoError(t, err)
-	assert.Equal(t, 1280, img.Bounds().Dx())
-	assert.True(t, img.Bounds().Dy() > 25_000)
 }
